fad8ad6ad314bef8d9b8ad3eba33bb7f0ffe6bcd
Michelly Menezes <martha.michelly@gmail.com>
2017-06-17 18:10:27 -0300

    retorna para versão anterior

--- app/src/main/java/com/projeto1/projeto1/MainActivity.java
+++ app/src/main/java/com/projeto1/projeto1/MainActivity.java
@@ -1,36 +1,37 @@
 package com.projeto1.projeto1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-import com.projeto1.projeto1.fragments.FacebookLoginFragment;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MAIN_ACTIVITY";
-    private FacebookLoginFragment facebookLoginFragment;
+    private TextView info;
+    private LoginButton loginButton;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        facebookLoginFragment = new FacebookLoginFragment();
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,11 +39,46 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                changeFragment(facebookLoginFragment.getInstance(), FacebookLoginFragment.TAG, true);
-
-
             }
         });
+
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.content_main);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -50,40 +86,6 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    public void changeFragment(Fragment frag, String tag, boolean saveInBackstack) {
-
-        try {
-            FragmentManager manager = getSupportFragmentManager();
-            //fragment not in back stack, create it.
-            FragmentTransaction transaction = manager.beginTransaction();
-
-
-            transaction.replace(R.id.content_layout, frag, tag);
-
-            if (saveInBackstack) {
-                Log.d(TAG, "Change Fragment: addToBackTack " + tag);
-                transaction.addToBackStack(tag);
-            } else {
-                Log.d(TAG, "Change Fragment: NO addToBackTack");
-            }
-            transaction.commit();
-            // custom effect if fragment is already instanciated
-
-        } catch (IllegalStateException exception) {
-            Log.w(TAG, "Unable to commit fragment, could be activity as been killed in background. " + exception.toString());
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        int fragments = getSupportFragmentManager().getBackStackEntryCount();
-        if (fragments == 1) {
-            finish();
-            return;
-        }
-        super.onBackPressed();
     }
 
     @Override
@@ -100,4 +102,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
