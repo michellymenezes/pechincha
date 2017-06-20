02865e08149ed374940d76684f8b32371fca66d4
rafaelle <rafaelle.xavier@gmail.com>
2017-06-20 08:54:00 -0300

    Conectando com o facebook e redirecionando para tela de promoções

--- app/src/main/java/com/projeto1/projeto1/MainActivity.java
+++ app/src/main/java/com/projeto1/projeto1/MainActivity.java
@@ -2,19 +2,17 @@ package com.projeto1.projeto1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -23,6 +21,7 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.projeto1.projeto1.fragments.LoginFragment;
 import com.projeto1.projeto1.fragments.MainFragment;
 
 import org.json.JSONException;
@@ -37,24 +36,114 @@ public class MainActivity extends AppCompatActivity {
     private TextView info;
     private LoginButton loginButton;
     private MainFragment myMainFragment;
-    private CallbackManager callbackManager;
+    private LoginFragment loginFragment;
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         myMainFragment = MainFragment.getInstance();
-        changeFragment(myMainFragment, MainFragment.TAG, true);
+        loginFragment = LoginFragment.getInstance();
+
+        if (AccessToken.getCurrentAccessToken() == null){
+            initializeLogin();
+            changeFragment(loginFragment, loginFragment.TAG, true);
+        } else {
+            Log.d(TAG, "Already logged");
+            changeFragment(myMainFragment, MainFragment.TAG, true);
+        }
+
         modifyActioonBar();
 
+    }
 
-/*
+    public void changeFragment(Fragment frag, String tag, boolean saveInBackstack) {
+
+        try {
+            FragmentManager manager = getSupportFragmentManager();
+            //fragment not in back stack, create it.
+            FragmentTransaction transaction = manager.beginTransaction();
+
+
+            transaction.replace(R.id.content_layout, frag, tag);
+
+            if (saveInBackstack) {
+                Log.d(TAG, "Change Fragment: addToBackTack " + tag);
+                transaction.addToBackStack(tag);
+            } else {
+                Log.d(TAG, "Change Fragment: NO addToBackTack");
+            }
+            transaction.commit();
+            // custom effect if fragment is already instanciated
+
+        } catch (IllegalStateException exception) {
+            Log.w(TAG, "Unable to commit fragment, could be activity as been killed in background. " + exception.toString());
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        int fragments = getSupportFragmentManager().getBackStackEntryCount();
+        if (fragments == 1) {
+            finish();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+//        setContentView(R.layout.test);
+    }
+
+    /**
+     * Inicia as definições da ActionBar para esse fragment
+     */
+    private void modifyActioonBar() {
+        android.support.v7.app.ActionBar mActionbar = (this.getSupportActionBar());
+        mActionbar.setTitle("");
+        mActionbar.setElevation(0);
+        mActionbar.setDisplayHomeAsUpEnabled(true);//if true displays the left menu
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.bag_button) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public CallbackManager initializeLogin(){
+
         FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
+        mCallbackManager = CallbackManager.Factory.create();
+        return mCallbackManager;
 
-        setContentView(R.layout.content_main);
+    }
+
+    private void login(CallbackManager callbackManager){
+        setContentView(R.layout.fragment_login);
         info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
         loginButton.setReadPermissions(Arrays.asList(
@@ -107,81 +196,6 @@ public class MainActivity extends AppCompatActivity {
                 info.setText("Login attempt failed.");
             }
         });
-        */
-        
-
-    }
-
-    public void changeFragment(Fragment frag, String tag, boolean saveInBackstack) {
-
-        try {
-            FragmentManager manager = getSupportFragmentManager();
-            //fragment not in back stack, create it.
-            FragmentTransaction transaction = manager.beginTransaction();
-
-
-            transaction.replace(R.id.content_layout, frag, tag);
-
-            if (saveInBackstack) {
-                Log.d(TAG, "Change Fragment: addToBackTack " + tag);
-                transaction.addToBackStack(tag);
-            } else {
-                Log.d(TAG, "Change Fragment: NO addToBackTack");
-            }
-            transaction.commit();
-            // custom effect if fragment is already instanciated
-
-        } catch (IllegalStateException exception) {
-            Log.w(TAG, "Unable to commit fragment, could be activity as been killed in background. " + exception.toString());
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        int fragments = getSupportFragmentManager().getBackStackEntryCount();
-        if (fragments == 1) {
-            finish();
-            return;
-        }
-        super.onBackPressed();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-//        setContentView(R.layout.test);
-    }
-
-    /**
-     * Inicia as definições da ActionBar para esse fragment
-     */
-    private void modifyActioonBar() {
-        android.support.v7.app.ActionBar mActionbar = (this.getSupportActionBar());
-        mActionbar.setTitle("");
-        mActionbar.setElevation(0);
-        mActionbar.setDisplayHomeAsUpEnabled(true);//if true displays the left menu
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.bag_button) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
---
+++ app/src/main/java/com/projeto1/projeto1/fragments/LoginFragment.java
@@ -0,0 +1,132 @@
+package com.projeto1.projeto1.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.projeto1.projeto1.MainActivity;
+import com.projeto1.projeto1.R;
+import com.projeto1.projeto1.adapters.CategorySwipeAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
+/**
+ * Created by rafaelle on 20/06/17.
+ */
+
+public class LoginFragment extends Fragment {
+
+    public static final String TAG = "LOGIN_FRAGMENT";
+    private View mView;
+    private LoginButton mLoginButton;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.2.
+     * @return A new instance of fragment FeedFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragment getInstance() {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        mView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        mLoginButton = (LoginButton) mView.findViewById(R.id.login_button);
+        mLoginButton.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
+
+        CallbackManager callbackManager = ((MainActivity) getActivity()).initializeLogin();
+        login(callbackManager, mLoginButton);
+
+        return mView;
+    }
+
+    private void login(CallbackManager callbackManager, LoginButton loginButton){
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG,
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken() +
+                                "\n"
+                );
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("LoginActivity", response.toString());
+
+                                // Application code
+                                try {
+                                    String email = object.getString("email");
+                                    String birthday = object.getString("birthday"); // 01/31/1980 format
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG,"Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.d(TAG,"Login attempt failed.");
+            }
+        });
+    }
+
+
+
+}
