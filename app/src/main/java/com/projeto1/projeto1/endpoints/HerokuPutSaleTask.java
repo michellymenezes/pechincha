aea6ce1ea0cb7964c87c349d45322a3f0a3a4088
Michelly Menezes <martha.michelly@gmail.com>
2017-08-28 16:25:28 -0300

    set to array

--- app/src/main/java/com/projeto1/projeto1/endpoints/HerokuPutSaleTask.java
+++ app/src/main/java/com/projeto1/projeto1/endpoints/HerokuPutSaleTask.java
@@ -69,7 +69,7 @@ public class HerokuPutSaleTask extends AsyncTask<Void, Void, Boolean> {
                     "&expirationDate=" +sale.getExpirationDate() +
                     "&author=" + sale.getAuthorId()  +
                     "&likeCount=" + sale.getLikeCount() + "&reportCount=" + sale.getReportCount() +
-                    "&likeUsers=" + sale.getLikeUsers() + "&reportUsers=" + sale.getReportUsers();
+                    "&likeUsers=" + sale.getLikeUsers().toArray() + "&reportUsers=" + sale.getReportUsers().toArray();
                     //+ "&historic="+ historic;
             url = new URL(ENDPOINT_ADDRESS);
 
